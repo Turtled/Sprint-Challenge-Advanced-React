ce7ae5c4666ac9ee720cf83dd49f6c54a1388003
Turtled <trtld2@gmail.com>
2019-10-04 09:41:48 -0700

    mvp complete

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import PlayerList from './components/PlayerList';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+afterEach(rtl.cleanup);
+
+it('renders correctly. truthy, in document, visible', () => {
+  const wrapper = rtl.render(
+    <PlayerList />
+  );
+  const element = wrapper.queryByText(/players/i);
+  expect(element).toBeTruthy();
+  expect(element).toBeInTheDocument();
+  expect(element).toBeVisible();
+
+  wrapper.debug();
 });
